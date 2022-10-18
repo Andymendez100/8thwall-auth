a07657cc81bfa85181690c820fe7b09104b738c2
Andy <andymendez100@gmail.com>
2022-10-17 22:40:08 -0700

    debugging helmet

--- server.js
+++ server.js
@@ -7,7 +7,22 @@ const PORT = process.env.PORT || 8080;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(helmet({ crossOriginResourcePolicy: { policy: "same-site" } }));
+// app.use(helmet({ crossOriginResourcePolicy: { policy: "same-site" } }));
+app.use(helmet.contentSecurityPolicy());
+app.use(helmet.crossOriginEmbedderPolicy());
+app.use(helmet.crossOriginOpenerPolicy());
+// app.use(helmet.crossOriginResourcePolicy());
+app.use(helmet.dnsPrefetchControl());
+app.use(helmet.expectCt());
+app.use(helmet.frameguard());
+app.use(helmet.hidePoweredBy());
+app.use(helmet.hsts());
+app.use(helmet.ieNoOpen());
+app.use(helmet.noSniff());
+app.use(helmet.originAgentCluster());
+app.use(helmet.permittedCrossDomainPolicies());
+app.use(helmet.referrerPolicy());
+app.use(helmet.xssFilter());
 app.use(
   cors({
     origin: 'https://8thwall.tonytnguyen.dev',
